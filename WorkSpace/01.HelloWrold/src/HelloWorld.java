013f3b177551c12a1fe516a8372692a07d3210cb
YmKim <77032646+youngmoon525@users.noreply.github.com>
2023-09-11 17:46:05 +0900

    자바 첫날 수업 완료
    main 메소드 ? : 여기에서 작업하라고함.
    syso : 콘솔창에 글자를 출력하는 명령

---
+++ WorkSpace/01.HelloWrold/src/HelloWorld.java
@@ -0,0 +1,13 @@
+// <- 주석. 기계가 컴파일 하지 않는 코드 즉 인간이 보기 위한 설명.
+// 1. main : 메인 메소드라고하며 우리가 당분간 계속 해서 사용할 프로그램의 시작점.
+// 2. syso : 프로그램이 진행 되는 중에 어떤 문자나 결과를 출력하기 위한 명령어.
+// 3. ctrl + space : 코드의 자동 완성.
+// 4. ctrl + s 또는 ctrl + shfit + s: 현재 파일 저장 , 전체 파일 저장
+
+// Q. 나의 이니셜Class를 하나 만들고 main메소드와 syso를 이용해서 콘솔에 내 이니셜이 출력 되게 
+// 코딩 해보기.
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorldKYM");
+	}
+}
